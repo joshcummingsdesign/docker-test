d25563541064fea2c0e006bd4029cadf0a46a5e0
josh <josh@madebygrizzly.com>
2016-08-30 03:50:11 -0700

    Added deploy script to be used as post-recieve hook

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function docker-cleanup {
+  EXITED=$(docker ps -aq -f status=exited)
+  DANGLING=$(docker images -q -f "dangling=true")
+
+  if [ -n "$EXITED" ]; then
+    echo "Removing these containers:"
+    docker rm $EXITED
+  else
+    echo "No containers to remove."
+  fi
+  if [ -n "$DANGLING" ]; then
+    echo "Removing these images:"
+    docker rmi $DANGLING
+  else
+    echo "No images to remove."
+  fi
+}
+
+docker-cleanup && \
+docker stop docker-test && \
+docker rm docker-test && \
+docker pull joshcummingsdesign/docker-test && \
+docker run --name docker-test -d -p 80:80 joshcummingsdesign/docker-test
